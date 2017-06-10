ef314deea7aecbf5063a8644d54665273baa7f38
gabbycorr <gaby.corr.developer@gmail.com>
2017-06-09 22:18:54 -0300

    Create 00_basicstruct.c

---
+++ 00_basicstruct.c
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------------------*/
+// LIBRARY
+
+	#include <stdio.h> // Help with some fuctions at another code
+
+/*-------------------------------------------------------------------------------------*/
+
+// MAIN CODE
+
+	int main (void){ // Point to compiler the star of program
+
+ 		printf("Hello World!); // Start printf library fuction
+	
+		return 0; // Value 0 return to main, means successfull termination
+	}
